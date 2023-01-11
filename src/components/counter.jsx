eda8683a5516a37ac21a59645383a5d8c6c8ef30
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-11 16:26:44 +0200

    14- Passing Event Arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,14 +5,9 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
-    // setState work:
-    //   1- calls the render method after a scheduled time (asynchronous 5ms or 10ms)
-    //   2- compares the new results and old results of render method
-    //   3- only updates the difference of the comparisiion
-    // So here just the span will update, not the whooole div element
-    // imagine there is a high quality images and they will time consuming to reload, why to re render them ?!!
   };
 
   render() {
@@ -20,7 +15,8 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
+          //onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
