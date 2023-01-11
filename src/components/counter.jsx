9d4110e3b4f3b7eacd0d45a867fd0d311525fa37
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-11 15:09:29 +0200

    7- Rendering Classes Dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,10 +7,16 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "primary" : "warning";
+    return classes;
   }
 
   formatCount() {
