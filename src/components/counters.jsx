e3de7da669f6654a214a57d7be3ff8b8cd8a2490
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-14 11:03:27 +0200

    9- Single Source of Truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -16,9 +16,23 @@ class Counters extends Component {
     this.setState({ counters });
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
