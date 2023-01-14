d9905f999bc87a821c6a547ea7838bdaad2494bc
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-14 13:43:08 +0200

    13 - Stateless Functional Components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar{" "}
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+// Stateless Functional Component
+const NavBar = (props) => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
