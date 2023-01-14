69a12413653cd68e80684dfd8d91722cabac6209
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-14 14:52:02 +0200

    14- Destructuring Arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,19 +3,18 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { counters, onIncrement, onReset, onDelete } = this.props;
+
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
             counter={counter}
           />
         ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 // Stateless Functional Component
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Navbar{" "}
           <span className="badge badge-pill badge-secondary">
-            {props.totalCounters}
+            {totalCounters}
           </span>
         </a>
       </div>
